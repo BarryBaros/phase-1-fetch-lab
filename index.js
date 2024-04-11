0a7e96245be76047fa0e7c7a2d05653d93cc9526
Timothy Barasa <timbaros87@gmail.com>
2024-04-11 17:18:43 +0300

    sends a fetch request and renders book titles into the DOM

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  // Make a GET request to the Game of Thrones API
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => {
+
+      // Convert the response to JSON
+      return resp.json()
+    })
+
+    .then(data => {
+      // Call the renderBooks() function with the JSON data
+      renderBooks(data);
+    });
 }
 
 function renderBooks(books) {
